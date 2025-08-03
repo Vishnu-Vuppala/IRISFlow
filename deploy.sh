e4db16f3670c984af30fc09c22f2448f2fb462d0
VishnuVuppala <vishnuvuppala0611@gmail.com>
2025-08-03 19:06:39 +0530

    shell updated

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker stop housing-api || true
 docker rm housing-api || true
 
 echo "Running new container..."
-docker run -d -p 8000:8000 --name housing-api yourdockerhubusername/housing-api:latest
+docker run -d -p 8000:8000 --name housing-api vishnuvuppala03/housing-api:latest
 echo "Container is running. You can access the API at http://localhost:8000"
